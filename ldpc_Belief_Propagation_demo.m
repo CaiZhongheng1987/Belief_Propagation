40f0a8006eb4b270d7ae4729f038d542d62e208c
CaiZhongheng1987 <38467143+CaiZhongheng1987@users.noreply.github.com>
2018-05-05 21:06:39 +0800

    Update ldpc_Belief_Propagation_demo.m

--- ldpc_Belief_Propagation_demo.m
+++ ldpc_Belief_Propagation_demo.m
@@ -1,18 +1,25 @@
 % c00194582 caizhongheng 2018.04.07
+% c00194582 caizhongheng 2018.05.05
+
 clear
 clc
 close all
-rng(257);
+rng(266);
 
-%% setting aera
-bit_grp = 1000;
-k = 6;
-m = 9;
-n = k + m;
+%% setting
+bit_grp     = 1000;
+k           = 6;
+m           = 9;
+n           = k + m;
+noise_scale = 2.4;% 调整噪声功率的比值，目的是让BPSK信号既出现纠前误码又不至于超出纠前门限
+iter_num    = 5; % 译码迭代次数
+dec_grp     = bit_grp;
+dec_mode    = 0; % 0: atanh mode; 1: minsum mode. 
+dec_out     = zeros(n,dec_grp);% 硬判输出结果
 
-Hb = [1 0 1 -1 -1; -1 0 0 0 -1;0 -1 -1 0 0];
-Z  = eye(3);
-H  = zeros(size(Hb,1)*length(Z),size(Hb,2)*length(Z));
+Hb          = [1 0 1 -1 -1; -1 0 0 0 -1;0 -1 -1 0 0];
+Z           = eye(3);
+H           = zeros(size(Hb,1)*length(Z),size(Hb,2)*length(Z));
 
 for idx_row = 1:size(Hb,1)
     for idx_col = 1:size(Hb,2)
@@ -26,6 +33,26 @@ end
 
 HI = H(:,1:k);
 HP = H(:,k+1:end);
+
+% 找出H矩阵中的非0元素，方便迭代用。
+[node_row,node_col] = find(H==1);
+node_array          = [node_row node_col];
+clear node_row node_col
+% C2V表示校验方程。
+C2V_slct            = zeros(size(H,1),length(find(node_array(:,1)==1)));
+for idx=1:size(C2V_slct,1)
+    % 找到H矩阵每行中为1的位置
+    Q_adress        = find(node_array(:,1)==idx);
+    % 将该位置存入Q_slct矩阵中
+    C2V_slct(idx,:) = node_array(Q_adress,2);
+end
+
+% V2C，行为变量节点，列为和该变量节点相连结的校验节点
+V2C_slct            = zeros(size(H,2),2);
+for idx=1:size(V2C_slct,1)
+    Q_adress        = find(node_array(:,2)==idx);
+    V2C_slct(idx,:) = node_array(Q_adress,1);
+end
 
 %% create the bit data in
 bit_in = randi([0 1],k*bit_grp,1);
@@ -43,64 +70,29 @@ enc_out = [bit_in;p];
 llr_data = 2*double(~enc_out)-1;
 % 因为llr=ln(APP(X=0)/APP(X=1))，这里为了简化llr的计算，所以对enc_out做了一个取反动作，然后直接映射成BPSK信号，作为LLR值。
 %% add noise
-noise = randn(size(llr_data))+1j*randn(size(llr_data));
-noise = noise/2.4;% 2.9是调整噪声功率的比值，目的是让BPSK信号既出现纠前误码又不至于超出纠前门限
+noise    = randn(size(llr_data))+1j*randn(size(llr_data));
+noise    = noise/noise_scale;
 tmp_data = llr_data + noise;
 
 scatterplot(tmp_data(:))
 llr_data = real(llr_data + noise);
 
 %% decode
-
-iter_num = 5; % 迭代次数
-dec_grp = bit_grp;
-
-
-% 找出H矩阵中的非0元素，方便迭代用。
-[node_row,node_col] = find(H==1);
-node_array = [node_row node_col];
-clear node_row node_col
-% C2V表示校验方程。
-C2V_slct     = zeros(size(H,1),length(find(node_array(:,1)==1)));
-for idx=1:size(C2V_slct,1)
-    % 找到H矩阵每行中为1的位置
-    Q_adress      = find(node_array(:,1)==idx);
-    % 将该位置存入Q_slct矩阵中
-    C2V_slct(idx,:) = node_array(Q_adress,2);
-end
-
-% V2C，行为变量节点，列为和该变量节点相连结的校验节点
-V2C_slct     = zeros(size(H,2),2);
-for idx=1:size(V2C_slct,1)
-    Q_adress = find(node_array(:,2)==idx);
-    V2C_slct(idx,:) = node_array(Q_adress,1);
-end
-
-
-
-
-
-llr_data_in = llr_data(:,1:dec_grp);% first col of data
-dec_out     = zeros(n,dec_grp);% 硬判输出结果
-llr_array = zeros(size(llr_data_in,1),iter_num);
-
-% keyboard;
-% llr_data_in(end) = 1.2;
+llr_data_in = llr_data(:,1:dec_grp);
+llr_array   = zeros(size(llr_data_in,1),iter_num);
 
 for grp_idx=1:dec_grp
     tmp_llr_data = llr_data_in(:,grp_idx);
-    
-    V = zeros(15,2);
-    APP = tmp_llr_data;
-    C = zeros(9,3);
-    tmp_dec_out = zeros(15,1);
+    V            = zeros(15,2);
+    APP          = tmp_llr_data;
+    C            = zeros(9,3);
+    tmp_dec_out  = zeros(15,1);
     
     for iter_idx=1:iter_num
         % 更新V的概率信息
         for idx=1:n
             [R_row,R_col] = find(C2V_slct==idx);
             C_slct        = [R_row,R_col];
-            %             V(idx) = APP(idx);
             if(idx<=12)
                 V(idx,1) = APP(idx) - C(C_slct(1,1),C_slct(1,2));
                 V(idx,2) = APP(idx) - C(C_slct(2,1),C_slct(2,2));
@@ -111,36 +103,32 @@ for grp_idx=1:dec_grp
         
         % 校验节点信息更新：Z1到Z9
         for idx=1:m
-            %             if(idx==m)
-            %                 keyboard;
-            %             end
             idx_array_1 = V2C_slct(C2V_slct(idx,1),:);
-            new_idx_1 = find(idx_array_1==idx,1);
+            new_idx_1   = find(idx_array_1==idx,1);
             idx_array_2 = V2C_slct(C2V_slct(idx,2),:);
-            new_idx_2 = find(idx_array_2==idx,1);
+            new_idx_2   = find(idx_array_2==idx,1);
             idx_array_3 = V2C_slct(C2V_slct(idx,3),:);
-            new_idx_3 = find(idx_array_3==idx,1);
+            new_idx_3   = find(idx_array_3==idx,1);
             
-            C(idx,1) = sign(V(C2V_slct(idx,2),new_idx_2))*sign(V(C2V_slct(idx,3),new_idx_3))*min(abs(V(C2V_slct(idx,2),new_idx_2)),abs(V(C2V_slct(idx,3),new_idx_3)));
-            C(idx,2) = sign(V(C2V_slct(idx,1),new_idx_1))*sign(V(C2V_slct(idx,3),new_idx_3))*min(abs(V(C2V_slct(idx,1),new_idx_1)),abs(V(C2V_slct(idx,3),new_idx_3)));
-            C(idx,3) = sign(V(C2V_slct(idx,1),new_idx_1))*sign(V(C2V_slct(idx,2),new_idx_2))*min(abs(V(C2V_slct(idx,1),new_idx_1)),abs(V(C2V_slct(idx,2),new_idx_2)));
-
-            %             C(idx,1) = 2*atanh(tanh(V(C2V_slct(idx,2))/2)*tanh(V(C2V_slct(idx,3))/2));
-            %             C(idx,2) = 2*atanh(tanh(V(C2V_slct(idx,1))/2)*tanh(V(C2V_slct(idx,3))/2));
-            %             C(idx,3) = 2*atanh(tanh(V(C2V_slct(idx,1))/2)*tanh(V(C2V_slct(idx,2))/2));
-            %             R_1(idx,1) = sign(V(C2V_slct(idx,2)))*sign(V(C2V_slct(idx,3)))*min(abs(V(C2V_slct(idx,2))),abs(V(C2V_slct(idx,3))));
-            %             R_1(idx,2) = sign(V(C2V_slct(idx,1)))*sign(V(C2V_slct(idx,3)))*min(abs(V(C2V_slct(idx,1))),abs(V(C2V_slct(idx,3))));
-            %             R_1(idx,3) = sign(V(C2V_slct(idx,1)))*sign(V(C2V_slct(idx,2)))*min(abs(V(C2V_slct(idx,2))),abs(V(C2V_slct(idx,2))));
+            if(dec_mode==0)
+                C(idx,1) = 2*atanh(tanh(V(C2V_slct(idx,2),new_idx_2)/2)*tanh(V(C2V_slct(idx,3),new_idx_3)/2));
+                C(idx,2) = 2*atanh(tanh(V(C2V_slct(idx,1),new_idx_1)/2)*tanh(V(C2V_slct(idx,3),new_idx_3)/2));
+                C(idx,3) = 2*atanh(tanh(V(C2V_slct(idx,1),new_idx_1)/2)*tanh(V(C2V_slct(idx,2),new_idx_2)/2));
+            else
+                C(idx,1) = sign(V(C2V_slct(idx,2),new_idx_2))*sign(V(C2V_slct(idx,3),new_idx_3))*min(abs(V(C2V_slct(idx,2),new_idx_2)),abs(V(C2V_slct(idx,3),new_idx_3)));
+                C(idx,2) = sign(V(C2V_slct(idx,1),new_idx_1))*sign(V(C2V_slct(idx,3),new_idx_3))*min(abs(V(C2V_slct(idx,1),new_idx_1)),abs(V(C2V_slct(idx,3),new_idx_3)));
+                C(idx,3) = sign(V(C2V_slct(idx,1),new_idx_1))*sign(V(C2V_slct(idx,2),new_idx_2))*min(abs(V(C2V_slct(idx,1),new_idx_1)),abs(V(C2V_slct(idx,2),new_idx_2)));
+            end
         end
         
-        % 变量节点信息更新：P1到P15
+        % 变量节点信息更新：c1到c15
         for idx=1:n
             [R_row,R_col] = find(C2V_slct==idx);
             C_slct        = [R_row,R_col];
             if(idx<=12)
-                APP(idx) = APP(idx) + C(C_slct(1,1),C_slct(1,2)) + C(C_slct(2,1),C_slct(2,2));
+                APP(idx)  = APP(idx) + C(C_slct(1,1),C_slct(1,2)) + C(C_slct(2,1),C_slct(2,2));
             else
-                APP(idx) = APP(idx) + C(C_slct(1,1),C_slct(1,2));
+                APP(idx)  = APP(idx) + C(C_slct(1,1),C_slct(1,2));
             end
             % 硬判
             if(APP(idx)>=0)
@@ -150,16 +138,11 @@ for grp_idx=1:dec_grp
             end
         end
         llr_array(:,iter_idx) = APP;
-        check_out = mod(H*tmp_dec_out,2);
-        check_out_sum = sum(check_out);
-        %         if(check_out_sum~=0)
-        %             keyboard
-        %         else
-        %         end
-        
+        check_out             = mod(H*tmp_dec_out,2);
+        check_out_sum         = sum(check_out);
     end
-    dec_out(:,grp_idx) = tmp_dec_out;
+    dec_out(:,grp_idx)        = tmp_dec_out;
 end
 
-check = abs(dec_out(1:15,:) - enc_out(1:15,1:dec_grp));
+check = abs(dec_out(1:n,:) - enc_out(1:n,1:dec_grp));
 sum(sum(check))
